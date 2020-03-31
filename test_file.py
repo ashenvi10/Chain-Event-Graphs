77280bf5b9015c7dc84961dcacad5e2d200f43a1
Aditi Shenvi <a.shenvi@warwick.ac.uk>
2020-03-31 08:58:35 +0100

    added tests to ensure results match with CEG in book

--- test_file.py
+++ test_file.py
@@ -18,6 +18,13 @@ def test_situations():
 def test_leaves():
     assert dataframe1.leaves == nodes[19:]
 
+def test_edge_countset():
+    assert dataframe1.edge_countset == [[507, 383], [237, 270], 
+    [46, 337], [86, 32, 119], [90, 65, 115], [14, 11, 21], 
+    [105, 158, 74], [70, 16], [24, 8], [109, 10], [76, 14], 
+    [49, 16], [104, 11], [12, 2], [8, 3], [18, 3], [75, 30], 
+    [117, 41], [59, 15]]
+
 def test_default_ess():
     assert dataframe1.default_equivalent_sample_size() == 3
 
@@ -37,10 +44,9 @@ def test_default_hyperstage():
 ceg_book_hyperstage = [['s0'], ['s1', 's2'], ['s3', 's4', 's5', 's6'],
 ['s7', 's8' ,'s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18']]
 def test_ceg_in_book_stages():
-    assert dataframe1.AHC_transitions(alpha =3, hyperstage = ceg_book_hyperstage)[0] == [['s3', 's5', 's6'],
-    ['s7','s11','s15','s16','s17'],['s8','s10','s12','s13','s18'],['s9','s14']]
+    assert dataframe1.AHC_transitions(alpha =3, hyperstage = ceg_book_hyperstage)[0] == [['s9', 's12'], 
+    ['s3', 's4', 's5'], ['s16', 's17', 's8', 's11', 's14'], 
+    ['s18', 's7', 's10', 's13', 's15']]
 
-#print(dataframe1.AHC_transitions(alpha = 3, hyperstage = [['s0'], ['s1', 's2'], ['s3', 's4', 's5', 's6'],['s7', 's8' ,'s9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18']]))
-print(dataframe1.edge_countset)
-#[[507, 383], [237, 270], [337, 46], [86, 119, 32], [158, 105, 74], [90, 65, 115], [14, 21, 11], [70, 16], [117, 41], [109, 10], [75, 30], [76, 14], [8, 24], [16, 49], [11, 104], [59, 15], [12, 2], [18, 3], [8, 3]]
-
+def test_ceg_in_book_loglikelihood():
+    assert round(dataframe1.AHC_transitions(alpha =3, hyperstage = ceg_book_hyperstage)[1],2) == -2478.49
